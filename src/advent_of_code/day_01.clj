51df52c807fbbf7649cdf6f5481b083f5c68c27b
Julian Jelfs <julian.jelfs@gmail.com>
2022-12-20 11:11:24 +0000

    day one

--- src/advent_of_code/day_01.clj
+++ src/advent_of_code/day_01.clj
@@ -1,11 +1,25 @@
-(ns advent-of-code.day-01)
+(ns advent-of-code.day-01
+  (:require [clojure.string :as str])
+)
+
+(defn sum-groups [group]
+  (reduce + (map read-string group))
+)
+
+(defn not-empty? [s]
+  (not (every? empty? s))
+)
+
+(defn calories_by_elf [input]
+   (map sum-groups (filter not-empty? (partition-by str/blank? (str/split-lines input))))
+)
 
 (defn part-1
   "Day 01 Part 1"
   [input]
-  input)
+   (apply max (calories_by_elf input)))
 
 (defn part-2
   "Day 01 Part 2"
   [input]
-  input)
+   (reduce + (take 3 (sort > (calories_by_elf input)))))
