78a6c4f4fc97b8d4a874f32c36b746c836170f8a
Julian Jelfs <julian.jelfs@gmail.com>
2022-12-20 21:52:09 +0000

    day4

--- src/advent_of_code/day_04.clj
+++ src/advent_of_code/day_04.clj
@@ -1,11 +1,39 @@
-(ns advent-of-code.day-04)
+(ns advent-of-code.day-04
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
+
+(defn to-range [[from to]] (set (range from (inc to))))
+
+(defn split-range [s]
+  (map read-string (str/split s #"-")))
+
+(defn to-ranges [line]
+  (->> (str/split line #",") (map split-range) (map to-range)))
+
+(defn overlap? [line]
+  (let [[one two] (to-ranges line)]
+        (or 
+          (empty? (set/difference one two))
+          (empty? (set/difference two one)))))
+
+(defn intersect? [line]
+  (let [[one two] (to-ranges line)]
+        (not (empty? (set/intersection one two)))))
+
+(defn solve
+  [input strategy]
+  (->> 
+    (str/split-lines input) 
+    (map strategy) 
+    (filter identity) 
+    count))
 
 (defn part-1
   "Day 04 Part 1"
   [input]
-  input)
+  (solve input overlap?))
 
 (defn part-2
   "Day 04 Part 2"
   [input]
-  input)
+  (solve input intersect?))
--- test/advent_of_code/day_04_test.clj
+++ test/advent_of_code/day_04_test.clj
@@ -1,11 +1,16 @@
 (ns advent-of-code.day-04-test
   (:require [clojure.test :refer [deftest testing is]]
-            [advent-of-code.day-04 :refer [part-1 part-2]]
+            [advent-of-code.day-04 :refer [part-1 part-2 to-ranges]]
             [clojure.java.io :refer [resource]]))
 
 (deftest part1
   (let [expected nil]
     (is (= expected (part-1 (slurp (resource "day-04-example.txt")))))))
+
+(deftest test-to-ranges
+  (let [expected nil]
+    (is (= expected (to-ranges ("2-6,4-8"))))))
+
 
 (deftest part2
   (let [expected nil]
