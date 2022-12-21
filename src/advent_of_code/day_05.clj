e00fb7897dbad80e3990eaf1cd8e62a285675721
Julian Jelfs <julian.jelfs@gmail.com>
2022-12-21 09:34:43 +0000

    day5

--- src/advent_of_code/day_05.clj
+++ src/advent_of_code/day_05.clj
@@ -1,11 +1,38 @@
-(ns advent-of-code.day-05)
+(ns advent-of-code.day-05
+  (:require [clojure.string :as str]))
+
+(def stacks [
+  [\p \v \z \w \d \t] 
+  [\d \j \f \v \w \s \l] 
+  [\h \b \t \v \s \l \m \z] 
+  [\j \s \r] 
+  [\w \l \m \f \g \b \z \c] 
+  [\b \g \r \z \h \v \w \q] 
+  [\n \d \b \c \p \j \v] 
+  [\q \b \t \p] 
+  [\c \r \z \g \h]
+])
+
+(defn parse-move "Format: move n from a to b" [line]
+  (map read-string (re-seq #"\d+" line)))
+
+(defn move [ordering stacks [n from to]]
+  (let [from-idx (dec from) 
+        to-idx (dec to)
+        from' (nth stacks from-idx)
+        to' (nth stacks to-idx) ]
+        (-> (assoc stacks from-idx (drop n from'))
+            (assoc to-idx (concat (ordering (take n from')) to')))))
+
+(defn solve [ordering input]
+  (map first (reduce (partial move ordering) stacks (map parse-move (str/split-lines input)))))
 
 (defn part-1
   "Day 05 Part 1"
   [input]
-  input)
+  (solve reverse input))
 
 (defn part-2
   "Day 05 Part 2"
   [input]
-  input)
+  (solve identity input))
