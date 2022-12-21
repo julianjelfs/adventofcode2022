d9b74b0015f210ea18de9e8278f32af511f927a8
Julian Jelfs <julian.jelfs@gmail.com>
2022-12-21 18:07:10 +0000

    day6

--- src/advent_of_code/day_06.clj
+++ src/advent_of_code/day_06.clj
@@ -1,11 +1,16 @@
 (ns advent-of-code.day-06)
 
+(defn find_marker [len pos input]
+  (if (== len (count (set (take len input)))) 
+    (+ pos len) 
+    (find_marker len (inc pos) (drop 1 input))))
+
 (defn part-1
   "Day 06 Part 1"
   [input]
-  input)
+  (find_marker 4 0 (seq input)))
 
 (defn part-2
   "Day 06 Part 2"
   [input]
-  input)
+  (find_marker 14 0 (seq input)))
